cbb9486449df55904757835adf779d2dcb26b526
ISHMEET008 <kishmeet008@gmail.com>
2025-03-30 15:23:36 +0530

    Solution to Digit Right-Shift Finder

--- Digit Right-Shift Finder@@67de00956ac3fec9afb90e13/code.c
+++ Digit Right-Shift Finder@@67de00956ac3fec9afb90e13/code.c
@@ -3,7 +3,7 @@ int digitRightShift(int n){
     int remaining_no=n/10;
     int final_no;
     final_no == lastdigit;
-    if(remaining_no==0){printf("%d",n)}
+    if(remaining_no==0){printf("%d",n);}
     printf("%d",final_no);
     printf("%d",remaining_no);
 }
