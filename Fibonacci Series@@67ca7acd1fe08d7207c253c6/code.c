c5affc854faf2b466bcb4b1f3038a375c7235347
ISHMEET008 <kishmeet008@gmail.com>
2025-03-30 17:26:16 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,10 +1,10 @@
-int fibonacciseries(int a){
+int fibonacciSeries(int a){
     if(a==1 || a==0)
     {   
         printf("1");
     }
     else{
-        int ans = fibonacciseries(a-1)+fibonacciseries(a-2);
+        int ans = fibonacciSeries(a-1)+fibonacciSeries(a-2);
         printf("%d",ans);
     }
 }
