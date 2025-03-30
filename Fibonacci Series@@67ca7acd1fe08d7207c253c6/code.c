2dbc87d2dbf2b563fb56b6224cce8b022fe9c63d
ISHMEET008 <kishmeet008@gmail.com>
2025-03-30 17:34:45 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -2,8 +2,8 @@ int fibonacciSeries(int n){
     int a=0;
     int b=1;
     int sum=1;
-    printf("%d %d",a,b);
-    for(int i=1;i<n;i++){
+    printf("%d %d ",a,b);
+    for(int i=1;i<=n;i++){
         sum=a+b;
         a=b;
         b=sum;
